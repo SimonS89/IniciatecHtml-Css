55948c6b964a6b6a067e8d1a85fba93c60dda186
SimonS89 <tripero89lp@gmail.com>
2022-09-13 22:58:02 -0300

    asd

--- bootstrap javascript/Simon Sueldo - Task 1/assets/scripts/index.js
+++ bootstrap javascript/Simon Sueldo - Task 1/assets/scripts/index.js
@@ -1,11 +1,10 @@
 import { datos, renderizar } from "./data.js";
+  const eventos = datos.eventos;
+  let cardsHome = document.getElementById("cards__home");
+  let template = "";
 
-let cardsHome = document.getElementById("cards__home");
-const eventos = datos.eventos;
-let template = "";
+  eventos.forEach((evento) => {
+    template += renderizar(evento);
+  });
 
-eventos.forEach((evento) => {
-  template += renderizar(evento);
-});
-
-cardsHome.innerHTML = template;
+  cardsHome.innerHTML = template;
