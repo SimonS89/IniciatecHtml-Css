146333edaa2cff9a9032c008e345bf1c38cbef26
SimonS89 <tripero89lp@gmail.com>
2022-09-22 18:18:35 -0300

    add eventos

--- bootstrap javascript/Simon Sueldo - Task 2/assets/scripts/index.js
+++ bootstrap javascript/Simon Sueldo - Task 2/assets/scripts/index.js
@@ -3,45 +3,134 @@ window.addEventListener("load", () => {
   const eventos = datos.eventos;
 
   //verson 1 js
-  let formContainer = document.querySelector(".form__categories");
-  let cardsHome = document.getElementById("cards__home");
-  let anioActual = datos.fechaActual;
-  let cardsPast = document.getElementById("cards__past");
-  let cardsUpcoming = document.getElementById("cards__upcoming");
+  const formContainer = document.querySelector(".form__categories");
+  const cardsHome = document.getElementById("cards__home");
+  const anioActual = datos.fechaActual;
+  const cardsPast = document.getElementById("cards__past");
+  const cardsUpcoming = document.getElementById("cards__upcoming");
+  let eventosPast = [];
+  let eventosUpcoming = [];
+  let categoriasFiltradas = [];
+  let eventosPorCategoria = [];
+  let eventosPorTexto = [];
 
+  //HOME
   if (cardsHome != null) {
     eventos.forEach((evento) => {
       renderizar(evento, cardsHome);
     });
     renderizarCategorias(formContainer);
+    let inputBusqueda = document.querySelector("#inputSearch");
+    inputBusqueda.addEventListener("keyup", function () {
+      mostrarFiltradosPorTexto(cardsHome, eventos, this.value);
+    });
+    let checkboxCategorias = document.querySelectorAll(
+      "input[name=categorias]"
+    );
+    checkboxCategorias.forEach((checkbox) => {
+      checkbox.addEventListener("click", function () {
+        mostrarFiltradosPorCategoria(cardsHome, this, eventos);
+      });
+    });
+    //PAST EVENTS
   } else if (cardsPast != null) {
     eventos.forEach((evento) => {
       let date = evento.date;
       if (date < anioActual) {
+        eventosPast.push(evento);
         renderizar(evento, cardsPast);
       }
     });
     renderizarCategorias(formContainer);
+    let inputBusqueda = document.querySelector("#inputSearch");
+    inputBusqueda.addEventListener("keyup", function () {
+      mostrarFiltradosPorTexto(cardsPast, eventosPast, this.value);
+    });
+    let checkboxCategorias = document.querySelectorAll(
+      "input[name=categorias]"
+    );
+    checkboxCategorias.forEach((checkbox) => {
+      checkbox.addEventListener("click", function () {
+        mostrarFiltradosPorCategoria(cardsPast, this, eventosPast);
+      });
+    });
+    //UPCOMING EVENTS
   } else {
     eventos.forEach((evento) => {
       let date = evento.date;
       if (date > anioActual) {
+        eventosUpcoming.push(evento);
         renderizar(evento, cardsUpcoming);
       }
     });
     renderizarCategorias(formContainer);
+    let inputBusqueda = document.querySelector("#inputSearch");
+    inputBusqueda.addEventListener("keyup", function () {
+      mostrarFiltradosPorTexto(cardsUpcoming, eventosUpcoming, this.value);
+    });
+    let checkboxCategorias = document.querySelectorAll(
+      "input[name=categorias]"
+    );
+    checkboxCategorias.forEach((checkbox) => {
+      checkbox.addEventListener("click", function () {
+        mostrarFiltradosPorCategoria(cardsUpcoming, this, eventosUpcoming);
+      });
+    });
   }
 
-  //   const botonBook = document.querySelectorAll("input[name=categorias]");
-  //   console.log(botonBook);
-  //   botonBook.addEventListener("click", function () {
-  //     console.log(this.checked);
-  //   });
-  //version varios js
-  // eventos.forEach((evento) => {
-  //   renderizar(evento, cardsHome);
-  // });
-  //   let myStr = "Hello World";
-  //   let firstWord = myStr.split(" ")[0];
-  //   console.log(firstWord);
+  //FUNCIONES AYUDA
+  function mostrarFiltradosPorCategoria(container, checkbox, eventos) {
+    container.innerHTML = "";
+    let eventosCategorias = filtrarEventosPorCategoria(eventos, checkbox);
+    eventosCategorias.forEach((evento) => {
+      renderizar(evento, container);
+    });
+  }
+
+  function mostrarFiltradosPorTexto(container, eventos, inputValue) {
+    container.innerHTML = "";
+    eventosPorTexto = filtrarPorTexto(eventos, inputValue);
+    eventosPorTexto.forEach((evento) => {
+      renderizar(evento, container);
+    });
+  }
+
+  function filtrarPorTexto(eventos, texto) {
+    let eventosValidosSearch = [];
+    eventosValidosSearch = eventos.filter((evento) => {
+      return evento.name.toLowerCase().includes(texto.toLowerCase());
+    });
+    return eventosValidosSearch;
+  }
+
+  function filtrarEventosPorCategoria(eventos, checkbox) {
+    if (checkbox.checked) {
+      categoriasFiltradas.push(checkbox.value);
+    } else if (
+      !checkbox.checked &&
+      categoriasFiltradas.includes(checkbox.value)
+    ) {
+      categoriasFiltradas.splice(
+        categoriasFiltradas.indexOf(checkbox.value),
+        1
+      );
+    }
+    eventosPorCategoria = [];
+    categoriasFiltradas.forEach(function (categoria) {
+      eventos.forEach(function (evento) {
+        if (evento.category.split(" ")[0] == categoria) {
+          eventosPorCategoria.push(evento);
+        }
+      });
+    });
+    if (eventosPorCategoria.length < 1) {
+      return eventos;
+    }
+    return eventosPorCategoria;
+  }
 });
+
+//version varios js
+// eventos.forEach((evento) => {
+//   renderizar(evento, cardsHome);
+// });
